422a505fe199513ad96d325724806daf3cee6b81
David M. Syzdek <david.syzdek@acsalaska.com>
2020-01-31 10:31:29 -0900

    client: starting work on client

--- src/akcom-udpecho.c
+++ src/akcom-udpecho.c
@@ -56,14 +56,28 @@
 ///////////////
 #pragma mark - Headers
 
+#include <stdint.h>
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <stdarg.h>
+#include <errno.h>
 #include <string.h>
 #include <strings.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 #include <time.h>
 #include <getopt.h>
 #include <assert.h>
+#include <syslog.h>
+#include <signal.h>
+#include <poll.h>
 
 
 ///////////////////
@@ -73,6 +87,16 @@
 ///////////////////
 #pragma mark - Definitions
 
+#ifndef PROGRAM_NAME
+#define PROGRAM_NAME "akcom-udpecho"
+#endif
+#ifndef PACKAGE_NAME
+#define PACKAGE_NAME "akcom-udpecho"
+#endif
+#ifndef PACKAGE_VERSION
+#define PACKAGE_VERSION "0.0"
+#endif
+
 
 /////////////////
 //             //
@@ -81,6 +105,24 @@
 /////////////////
 #pragma mark - Datatypes
 
+union my_sa
+{
+   struct sockaddr         sa;
+   struct sockaddr_in      sin;
+   struct sockaddr_in6     sin6;
+   struct sockaddr_storage ss;
+};
+
+
+struct udp_echo_plus
+{
+   uint32_t  req_sn;
+   uint32_t  res_sn;
+   uint32_t  recv_time;
+   uint32_t  reply_time;
+   uint32_t  failures;
+};
+
 
 /////////////////
 //             //
@@ -88,6 +130,13 @@
 //             //
 /////////////////
 #pragma mark - Variables
+
+static const char    * prog_name    = PROGRAM_NAME;
+static int             cnf_echoplus = 0;
+static int             cnf_verbose  = 0;
+static time_t          cnf_timeout  = 5;
+static short           cnf_port     = 30006;
+static const char    * cnf_host     = NULL;
 
 
 //////////////////
@@ -100,6 +149,12 @@
 // main statement
 int main(int argc, char * argv[]);
 
+// display program usage
+void my_usage(void);
+
+// display program usage error
+void my_usage_error(const char * fmt, ...);
+
 
 /////////////////
 //             //
@@ -111,9 +166,123 @@ int main(int argc, char * argv[]);
 // main statement
 int main(int argc, char * argv[])
 {
-   if (argc < 0)
-      printf("hello %s\n", argv[0]);
+   int                       c;
+   int                       opt_index;
+   char                    * ptr;
+   union my_sa               sa;
+
+   // getopt options
+   static char   short_opt[] = "d:D:efg:hl:np:P:ru:vV";
+   static struct option long_opt[] =
+   {
+      {"echoplus",      no_argument,       0, 'e'},
+      {"help",          no_argument,       0, 'h'},
+      {"rfc",           no_argument,       0, 'r'},
+      {"verbose",       no_argument,       0, 'v'},
+      {"version",       no_argument,       0, 'V'},
+      {NULL,            0,                 0, 0  }
+   };
+
+   // determines program name
+   prog_name = argv[0];
+   if ((ptr = rindex(argv[0], '/')) != NULL)
+      prog_name = &ptr[1];
+
+   // process arguments
+   while((c = getopt_long(argc, argv, short_opt, long_opt, &opt_index)) != -1)
+   {
+      switch(c)
+      {
+         case -1:       // no more arguments
+         case 0:        // long options toggles
+         break;
+
+         case 'e':
+         cnf_echoplus = 1;
+         break;
+
+         case 'E':
+         cnf_echoplus = 0;
+         break;
+
+         case 'h':
+         my_usage();
+         return(0);
+
+         case 'v':
+         cnf_verbose++;
+         break;
+
+         case 'V':
+         printf("%s (%s) %s\n", prog_name, PACKAGE_NAME, PACKAGE_VERSION);
+         return(0);
+
+         case '?':
+         fprintf(stderr, "Try `%s --help' for more information.\n", prog_name);
+         return(1);
+
+         default:
+         my_usage_error("unrecognized option `--%c'", c);
+         return(1);
+      };
+   };
+   if (optind >= argc)
+   {
+      my_usage_error("missing remote host");
+      return(1);
+   };
+   cnf_host = argv[optind++];
+   if (optind < argc)
+   {
+      cnf_port = strtoul(argv[optind++], &ptr, 10);
+      if ( (!(cnf_port)) || ((ptr[0])) )
+      {
+         my_usage_error("invalid port number");
+         return(1);
+      };
+   };
+   if (optind < argc)
+   {
+      my_usage_error("unknown argument `%s'", argv[optind++]);
+      return(1);
+   };
+
+printf("connect to %s:%hu\n", cnf_host, cnf_port);
+
    return(0);
+}
+
+
+// display program usage
+void my_usage(void)
+{
+   printf("Usage: %s [options] host [port]\n", prog_name);
+   printf("OPTIONS:\n");
+   printf("  -e,      --echoplus       enable echo plus, not RFC compliant%s\n", ((cnf_echoplus)) ? " (default)" : "");
+   printf("  -h,      --help           print this help and exit\n");
+   printf("  -r,      --rfc            RFC compliant echo protocol%s\n", (!(cnf_echoplus)) ? " (default)" : "");
+   printf("  -t sec                    response timeout (default: %lu s)\n", cnf_timeout);
+   printf("  -v,      --verbose        enable verbose output\n");
+   printf("  -V,      --version        print version number and exit\n");
+   printf("\n");
+   return;
+}
+
+
+// display program usage error
+void my_usage_error(const char * fmt, ...)
+{
+   va_list args;
+
+   fprintf(stderr, "%s: ", prog_name);
+
+   va_start(args, fmt);
+   vfprintf(stderr, fmt, args);
+   va_end(args);
+
+   fprintf(stderr, "\nTry `%s --help' for more information.\n", prog_name);
+
+   return;
 }
 
 
