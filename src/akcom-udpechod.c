87355226022accb68e7899374fdc07870ad77a35
David M. Syzdek <david.syzdek@acsalaska.com>
2022-07-21 15:40:35 -0800

    akcom-udpechod: cleaning up logging for dropped or invalid packets

--- src/akcom-udpechod.c
+++ src/akcom-udpechod.c
@@ -757,19 +757,37 @@ int my_log_conn(int mode, size_t * connp, union my_sa * sap,
    // log connection
    if ((cnf_echoplus))
    {
-      syslog(LOG_INFO,
-         "conn %zu: client: [%s]:%hu; %s bytes: %zi; timestamp: %lu.%09lu; seq: %u; delta: %u us; delay: %u us;",
-         *connp,
-         addr_str,
-         port,
-         mode_name,
-         ssize,
-         tsp->tv_sec,
-         tsp->tv_nsec,
-         ntohl(msgp->req_sn),
-         ntohl(msgp->reply_time) - ntohl(msgp->recv_time),
-         delay
-      );
+      if (mode == MY_SENT)
+      {
+         syslog(LOG_INFO,
+            "conn %zu: client: [%s]:%hu; %s bytes: %zi; timestamp: %lu.%09lu; seq: %u; delay: %u.%03u ms; delta: %u.%03u ms;",
+            *connp,
+            addr_str,
+            port,
+            mode_name,
+            ssize,
+            tsp->tv_sec,
+            tsp->tv_nsec,
+            ntohl(msgp->req_sn),
+            (delay/1000),
+            (delay%1000),
+            (ntohl(msgp->reply_time) - ntohl(msgp->recv_time))/1000,
+            (ntohl(msgp->reply_time) - ntohl(msgp->recv_time))%1000
+         );
+      } else
+      {
+         syslog(LOG_INFO,
+            "conn %zu: client: [%s]:%hu; %s bytes: %zi; timestamp: %lu.%09lu; seq: %u;",
+            *connp,
+            addr_str,
+            port,
+            mode_name,
+            ssize,
+            tsp->tv_sec,
+            tsp->tv_nsec,
+            ntohl(msgp->req_sn)
+         );
+      };
    } else
    {
       syslog(LOG_INFO,
